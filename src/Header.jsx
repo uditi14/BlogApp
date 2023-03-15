4d24d18423bfb84db64089ce126daf7448900474
uditi14 <uditidt@gmail.com>
2023-03-15 17:08:37 +0530

    Second Commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.jpg'
+
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} className='logo'></img>
+            <nav>
+                <a href=''>Login</a>
+                <a href=''>Register</a>
+            </nav>
+        </header>
+    )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,19 @@
+import randomImg from './assets/image.png'
+
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src={randomImg}></img>
+        </div>
+        <div className="text">
+          <h2>The Importance of Vocabulary</h2>
+          <p className="info">
+            <a className="author">John Doe</a>
+            <time>2023-01-15 13:39</time>
+          </p>
+          <p className='summary'>Vocabulary is the knowledge of words, and vocabulary words are a specific set of words to be learned. Vocabulary is an essential component of reading comprehension.</p>
+        </div>
+      </div>
+    )
+}
