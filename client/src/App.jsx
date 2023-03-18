aeefefdac131c506c77c9708db06df285afadef9
Viveksharma121 <114184360+Viveksharma121@users.noreply.github.com>
2023-03-18 13:36:35 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,10 @@ import IndexPage from "./pages/IndexPage.jsx";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import RegLogin from "./pages/RegLogin";
-import { UserContextProvider } from "./UserContext";
+import { UserContextProvider } from "./UserContex";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path={"/login"} element={<RegLogin />} />
           <Route path={"/register"} element={<RegLogin />} />
           <Route path={"/create"} element={<CreatePost />} />
-          <Route path={"/post/:id"} element={<PostPage/>} />
+          <Route path={"/post/:id"} element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
         </Route>
       </Routes>
     </UserContextProvider>
@@ -24,4 +26,3 @@ function App() {
 }
 
 export default App;
-      
